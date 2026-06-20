49f1e794a14f45ffbf0913593f13d13a94ee7144
baieltaalaiuulu-create <baieltaalaiuulu@gmail.com>
2026-06-20 15:17:04 +0600

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
 };
 
